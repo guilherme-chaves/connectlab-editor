f62d1f58988a0b7b0e801b057ec85cda5a6a2762
Guilherme Chaves <guilherme.chaves01@gmail.com>
2024-06-05 09:57:03 -0300

    Update editor.ts
    Melhor tratamento de erros e adicionar funcionalidade para dar o usuário especificar o nome do arquivo a ser baixado

--- src/functions/misc/editor.ts
+++ src/functions/misc/editor.ts
@@ -12,14 +12,33 @@ export function loadFile(
     return;
   }
   const input = ev.target as HTMLInputElement;
-  if (!input.files) return;
+  if (!input.files || !input.files[0]) return;
   const reader = new FileReader();
   reader.readAsArrayBuffer(input.files[0]);
   reader.onload = () => {
     if (!reader.result || typeof reader.result === 'string') return;
     const unzipped = gunzipSync(new Uint8Array(reader.result));
     const jsonData = JSON.parse(new TextDecoder().decode(unzipped));
-    // if (!(jsonData instanceof EditorEnvironmentObject))
+    if (
+      typeof jsonData.id !== 'string' &&
+      typeof jsonData.data.nodes !== 'object' &&
+      Number.isInteger(jsonData.data.nodes.length) &&
+      typeof jsonData.data.connections !== 'object' &&
+      Number.isInteger(jsonData.data.connections.length) &&
+      typeof jsonData.data.slots !== 'object' &&
+      Number.isInteger(jsonData.data.slots.length) &&
+      typeof jsonData.data.texts !== 'object' &&
+      Number.isInteger(jsonData.data.texts.length) &&
+      typeof jsonData.signal !== 'object'
+    ) {
+      window.alert(
+        'Falha ao carregar projeto! Arquivo inválido ou corrompido.'
+      );
+      console.error(
+        'Falha ao carregar projeto! Arquivo inválido ou corrompido.'
+      );
+      return;
+    }
     editor.editorEnv = EditorEnvironment.createFromJson(
       jsonData,
       ctx,
@@ -29,25 +48,36 @@ export function loadFile(
 }
 
 export function saveToFile(editorEnv: EditorEnvironment) {
+  const fileName = window.prompt(
+    'Salvar projeto como:',
+    `${editorEnv.documentId}-${Date.now()}`
+  );
+  if (fileName === null) return;
   const a = document.createElement('a');
   const file = new TextEncoder().encode(editorEnv.saveAsJson());
   const compressed = gzipSync(file, {level: 6});
   a.href = URL.createObjectURL(
     new Blob([compressed], {type: 'application/gzip'})
   );
-  a.download = `${editorEnv.documentId}-save-${Date.now()}.save`;
+  a.download = `${fileName}.save`;
   a.click();
 }
 
-export function clearEditor(editorEnv: EditorEnvironment) {
-  editorEnv.nodes.clear();
-  editorEnv.connections.clear();
-  editorEnv.slots.clear();
-  editorEnv.texts.clear();
-  editorEnv.signalGraph = {};
-  editorEnv = new EditorEnvironment(
-    editorEnv.documentId,
-    0,
-    editorEnv.nodeImageList
-  );
+export function clearEditor(editorEnv: EditorEnvironment): void {
+  if (
+    confirm(
+      'Deseja limpar o editor?\nQualquer progresso não salvo será perdido!'
+    ) === true
+  ) {
+    editorEnv.nodes.clear();
+    editorEnv.connections.clear();
+    editorEnv.slots.clear();
+    editorEnv.texts.clear();
+    editorEnv.signalGraph = {};
+    editorEnv = new EditorEnvironment(
+      editorEnv.documentId,
+      0,
+      editorEnv.nodeImageList
+    );
+  }
 }
