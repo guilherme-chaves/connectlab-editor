bc2c19da1cef63bcf59dd4a31c1b3655d8441e23
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-01-14 22:14:53 -0300

    Corrigir erro do caminho dos arquivos ao rodar o script build

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: './',
   test: {
     setupFiles: ['./vitest.setup.mjs'],
     environment: 'happy-dom',
