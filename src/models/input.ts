50a15283f1313500c0270595082b8c1cb487a6c1
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-03-13 19:23:28 -0300

    Modelo do componente ButtonInput

--- src/models/input.ts
+++ src/models/input.ts
@@ -2,6 +2,8 @@ import Vector2 from '@connectlab-editor/types/vector2';
 import {NodeModel} from '@connectlab-editor/types/common';
 import INPUT_OFF from '@connectlab-editor/gates/INPUT_OFF.svg';
 import INPUT_ON from '@connectlab-editor/gates/INPUT_ON.svg';
+import BUTTON_OFF from '@connectlab-editor/assets/gates/BUTTON_OFF.svg';
+import BUTTON_ON from '@connectlab-editor/assets/gates/BUTTON_ON.svg';
 import {NodeTypes} from '@connectlab-editor/types/enums';
 
 export const SwitchInput: NodeModel = {
@@ -11,7 +13,20 @@ export const SwitchInput: NodeModel = {
     {
       id: 0,
       in: false,
-      name: 'A',
+      name: 'Out',
+      localPos: new Vector2(70, 25),
+    },
+  ],
+};
+
+export const ButtonInput: NodeModel = {
+  id: NodeTypes.I_BUTTON,
+  imgPath: [BUTTON_OFF, BUTTON_ON],
+  connectionSlot: [
+    {
+      id: 0,
+      in: false,
+      name: 'Out',
       localPos: new Vector2(70, 25),
     },
   ],
