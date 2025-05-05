1b5eaed0c6d9a987e420998ede6b68890fbce22c
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-05-05 20:46:39 -0300

    Alterar nomes de variáveis da classe Mouse

--- src/types/mouse.ts
+++ src/types/mouse.ts
@@ -2,42 +2,45 @@ import Vector2 from '@connectlab-editor/types/vector2';
 
 export default class Mouse {
   public position = new Vector2();
-  private _mouseClicked = false;
+  private mouseClicked = false;
   public doubleClicked = false;
   public clickStartPosition = new Vector2();
-  private _mouseStateChanged = false;
+  private mouseStateChanged = false;
   public readonly clickToDragThreshold = 6; // pixels
   public readonly doubleClickTimer = 300; // ms
   private alreadyClicked = false;
+  private mouseDragged = false;
 
   get clicked(): boolean {
-    return this._mouseClicked;
+    return this.mouseClicked;
   }
 
   set clicked(value: boolean) {
-    this._mouseClicked = value;
-    this._mouseStateChanged = true;
+    this.mouseClicked = value;
+    this.mouseStateChanged = true;
     if (value) this.clickStartPosition = this.position.copy();
+    else this.mouseDragged = false;
   }
 
   get dragged(): boolean {
     const mouseMovement = Vector2.sub(this.position, this.clickStartPosition);
-    return (
+    this.mouseDragged =
+      this.mouseDragged ||
       mouseMovement.x > this.clickToDragThreshold ||
       mouseMovement.x < -this.clickToDragThreshold ||
       mouseMovement.y > this.clickToDragThreshold ||
-      mouseMovement.y < -this.clickToDragThreshold
-    );
+      mouseMovement.y < -this.clickToDragThreshold;
+    return this.mouseDragged;
   }
 
   get stateChanged(): boolean {
-    return this._mouseStateChanged;
+    return this.mouseStateChanged;
   }
 
   set stateChanged(value: boolean) {
-    this._mouseStateChanged = value;
+    this.mouseStateChanged = value;
     if (this.alreadyClicked) {
-      this.doubleClicked = this._mouseClicked;
+      this.doubleClicked = this.mouseClicked;
     } else {
       this.alreadyClicked = true;
       setTimeout(() => (this.alreadyClicked = false), this.doubleClickTimer);
