9b5fdf3811760beea5b5fab164402ac7035e86e9
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-05-13 19:04:40 -0300

    PathFinder - Início da lógica de busca complexa

--- src/functions/connection/pathFinder.ts
+++ src/functions/connection/pathFinder.ts
@@ -3,6 +3,15 @@ import {NodeList} from '@connectlab-editor/types/common';
 import * as angles from '@connectlab-editor/types/consts';
 import Vector2 from '@connectlab-editor/types/vector2';
 
+type scoreList = Record<
+  string, // x:y
+  {
+    t: Vector2;
+    fromT: Vector2 | null;
+    score: number;
+  }
+>;
+
 export default {
   stepDirectionFromAtan2(atan2: number): Vector2 {
     const result = new Vector2(0, 0, false);
@@ -101,6 +110,88 @@ export default {
       Vector2.copy(nextT, currentT);
     }
     return list;
+  },
+  stepCollisionExists(
+    start: Vector2,
+    end: Vector2,
+    current: Vector2,
+    nextT: Vector2,
+    nodeList: NodeList
+  ): boolean {
+    const next = Vector2.bilinear(start, end, nextT);
+    for (const node of nodeList.values()) {
+      if (node.collisionShape.collisionWithLine(current, next)) return true;
+    }
+    return false;
+  },
+  manhattanDistance(p1: Vector2, p2: Vector2): number {
+    return Math.abs(p1.x - p2.x) + Math.abs(p1.y + p2.y);
+  },
+  computeStepScore(start: Vector2, current: Vector2, end: Vector2): number {
+    return (
+      this.manhattanDistance(start, current) +
+      this.manhattanDistance(current, end)
+    );
+  },
+  createPathFromScores(scores: scoreList): Array<Vector2> {
+    let current = scores['1:1'] ?? null;
+    const list: Array<Vector2> = [];
+    if (current === null) return [];
+    while (current.fromT !== null) {
+      list.unshift(current.t);
+      current = scores[`${current.fromT.x}:${current.fromT.y}`];
+    }
+    return list;
+  },
+  complexPathFinder(
+    start: Vector2,
+    end: Vector2,
+    nodeList: NodeList,
+    stepDivisor: Vector2 = new Vector2(2, 1)
+  ) {
+    const list: Array<Vector2> = [];
+    if (start.equals(end)) return list; // Não calcular se os vetores forem iguais
+    const current = start.copy();
+    const currentT = new Vector2(0, 0, false);
+    const scores: Record<
+      string, // x:y
+      {
+        t: Vector2;
+        fromT: Vector2 | null;
+        score: number;
+      }
+    > = {};
+    scores['0:0'] = {
+      t: new Vector2(0, 0, false),
+      fromT: null,
+      score: this.computeStepScore(start, start, end),
+    };
+    let runs = 0;
+    while (!current.equals(end)) {
+      if (runs > 127) break;
+      runs++;
+      const atan2 = end.atan2(current);
+      const desiredStep = this.stepDirectionFromAtan2(atan2);
+      const nextT = desiredStep.abs().div(stepDivisor).add(currentT);
+      if (!this.stepCollisionExists(start, end, current, nextT, nodeList)) {
+        const next = Vector2.bilinear(start, end, nextT, undefined, true);
+        const nextScore = this.computeStepScore(start, current, end);
+        if (
+          scores[`${nextT.x}:${nextT.y}`] === undefined ||
+          scores[`${nextT.x}:${nextT.y}`].score > nextScore
+        ) {
+          scores[`${nextT.x}:${nextT.y}`] = {
+            t: nextT,
+            fromT: currentT,
+            score: nextScore,
+          };
+        }
+        Vector2.copy(next, current);
+        Vector2.copy(nextT, currentT);
+        continue;
+      }
+    }
+    return this.createPathFromScores(scores);
   },
   // Função principal
   find(start: Vector2, end: Vector2, nodeList: NodeList) {
