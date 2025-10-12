3aa3e11e5c8c6dd032f79305656e1136a8d5c65d
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-10-12 15:40:53 -0300

    Adicionar mais configurações ao ESLint e tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,47 @@
 import { defineConfig } from 'eslint/config';
-import js from '@eslint/js';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  stylistic.configs.recommended,
   {
     files: ['**/*.js', '**/*.mjs', '**/*.ts'],
     plugins: {
       '@stylistic': stylistic,
-      js,
     },
-    extends: ['js/recommended'],
     rules: {
-      '@stylistic/semi': 'error',
+      '@stylistic/semi': ['error', 'always'],
       '@stylistic/eol-last': 'error',
       'eqeqeq': 'error',
       'no-var': 'error',
       'prefer-const': 'error',
       'prefer-arrow-callback': 'error',
       '@stylistic/no-trailing-spaces': 'error',
-      '@stylistic/quotes': ['warn', 'single', {'avoidEscape': true}],
+      '@stylistic/quotes': ['warn', 'single', { avoidEscape: true }],
       'indent': ['error', 2],
       '@stylistic/dot-location': ['error', 'property'],
       '@stylistic/brace-style': 'error',
       '@stylistic/linebreak-style': ['error', 'unix'],
-      '@stylistic/max-len': ['error', { 'code': 80 }],
+      '@stylistic/max-len': ['error', { code: 80 }],
       '@stylistic/no-confusing-arrow': 'error',
       '@stylistic/no-extra-parens': [
         'error',
         'all',
         {
-          'nestedBinaryExpressions': false
-        }
+          nestedBinaryExpressions: false,
+        },
       ],
       '@stylistic/no-mixed-operators': 'error',
       '@stylistic/no-mixed-spaces-and-tabs': 'error',
       '@stylistic/no-extra-semi': 'error',
       '@stylistic/no-multi-spaces': 'error',
-      'no-restricted-properties': [
-        'error',
-        {
-          'object': 'describe',
-          'property': 'only'
-        },
-        {
-          'object': 'it',
-          'property': 'only'
-        }
-      ]
+      '@stylistic/type-annotation-spacing': 'error',
+      '@stylistic/type-generic-spacing': ['error'],
+      '@stylistic/type-named-tuple-spacing': ['error'],
     },
-    ignores: ['build/']
-  }
+    ignores: ['build/', 'node_modules/', 'dist/', '*.json'],
+  },
 ]);
