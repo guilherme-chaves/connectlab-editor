8b90a66f1c691a5dea03c16cb2313c90d3825b7c
Guilherme Chaves <guilherme.chaves01@gmail.com>
2025-10-26 22:11:53 -0300

    Adicionar tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
 import './style.css';
 import Editor from '@connectlab-editor/editor';
+import ModalController from './modal';
 
+new ModalController(
+  'app-modal-tutorial',
+  'tutorial-button',
+  'app-modal-close',
+  'app-tutorial-skip',
+  'app-tutorial-next',
+  'app-tutorial-previous',
+  'app-tutorial-close',
+);
 new Editor(uuidv4(), 'Teste', 'editor-canvas', 'background-canvas');
---
+++ src/modal.ts
@@ -0,0 +1,135 @@
+export default class ModalController {
+  public modalDOM: HTMLElement | null;
+  public openButton: HTMLButtonElement | null;
+  public closeButton: HTMLElement | null;
+  public skipButton: HTMLButtonElement | null;
+  public nextButton: HTMLButtonElement | null;
+  public previousButton: HTMLButtonElement | null;
+  public tutorialEndButton: HTMLButtonElement | null;
+  private currentCarrouselId: number;
+  private carrouselMaxId: number;
+  public carrouselItems: HTMLCollectionOf<Element>;
+
+  constructor(
+    modalId: string,
+    openModalId: string,
+    closeModalId: string,
+    skipButtonId: string,
+    nextButtonId: string,
+    previousButtonId: string,
+    tutorialEndButtonId: string,
+  ) {
+    this.modalDOM = document.getElementById(modalId);
+    this.openButton = this.checkHTMLButtonElement(
+      document.getElementById(openModalId),
+    );
+    this.skipButton = this.checkHTMLButtonElement(
+      document.getElementById(skipButtonId),
+    );
+    this.nextButton = this.checkHTMLButtonElement(
+      document.getElementById(nextButtonId),
+    );
+    this.previousButton = this.checkHTMLButtonElement(
+      document.getElementById(previousButtonId),
+    );
+    this.tutorialEndButton = this.checkHTMLButtonElement(
+      document.getElementById(tutorialEndButtonId),
+    );
+    this.closeButton = document.getElementById(closeModalId);
+    this.currentCarrouselId = 0;
+    this.carrouselItems = document.getElementsByClassName('carrousel-item');
+    this.carrouselMaxId = this.carrouselItems.length - 1;
+    if (this.carrouselItems.length > 0) {
+      this.carrouselItems[0].classList.add('carrousel-active');
+    }
+    this.createEvents();
+  }
+
+  private checkHTMLButtonElement(
+    el: HTMLElement | null,
+  ): HTMLButtonElement | null {
+    if (el !== null && el.tagName === 'BUTTON')
+      return el as HTMLButtonElement;
+    return null;
+  }
+
+  createEvents() {
+    if (
+      this.modalDOM !== null
+      && this.openButton !== null
+    ) {
+      this.openButton.addEventListener('click', () => {
+        this.modalDOM!.style.display = 'block';
+      });
+      if (this.closeButton) {
+        this.closeButton.addEventListener('click', () => {
+          this.modalDOM!.style.display = 'none';
+        });
+      }
+    }
+    if (
+      this.modalDOM !== null
+      && this.skipButton !== null
+      && this.nextButton !== null
+      && this.previousButton !== null
+      && this.tutorialEndButton !== null
+    ) {
+      this.previousButton.style.display = 'none';
+      this.tutorialEndButton.style.display = 'none';
+      this.skipButton.addEventListener('click', () => {
+        this.carrouselPrevious(0);
+        this.modalDOM!.style.display = 'none';
+      });
+      this.previousButton.addEventListener('click', () => {
+        this.carrouselPrevious(this.currentCarrouselId - 1);
+      });
+      this.nextButton.addEventListener('click', () => {
+        this.carrouselNext(this.currentCarrouselId + 1);
+      });
+      this.tutorialEndButton.addEventListener('click', () => {
+        this.carrouselPrevious(0);
+        this.modalDOM!.style.display = 'none';
+      });
+    }
+  }
+
+  carrouselNext(id: number) {
+    if (
+      this.nextButton === null
+      || this.previousButton === null
+      || this.skipButton === null
+      || this.tutorialEndButton === null
+    )
+      return;
+    this.carrouselItems[this.currentCarrouselId].classList.remove('carrousel-active');
+    this.currentCarrouselId = id;
+    if (this.currentCarrouselId >= this.carrouselMaxId) {
+      this.currentCarrouselId = this.carrouselMaxId;
+      this.nextButton.style.display = 'none';
+      this.tutorialEndButton.style.display = 'block';
+    }
+    this.carrouselItems[this.currentCarrouselId].classList.add('carrousel-active');
+    this.previousButton.style.display = 'block';
+    this.skipButton.style.display = 'none';
+  }
+
+  carrouselPrevious(id: number) {
+    if (
+      this.nextButton === null
+      || this.previousButton === null
+      || this.skipButton === null
+      || this.tutorialEndButton === null
+    )
+      return;
+    this.carrouselItems[this.currentCarrouselId].classList.remove('carrousel-active');
+    this.currentCarrouselId = id;
+    if (this.currentCarrouselId <= 0) {
+      this.currentCarrouselId = 0;
+      this.previousButton.style.display = 'none';
+      this.skipButton.style.display = 'block';
+    }
+    this.tutorialEndButton.style.display = 'none';
+    this.carrouselItems[this.currentCarrouselId].classList.add('carrousel-active');
+    this.nextButton.style.display = 'block';
+  }
+}
